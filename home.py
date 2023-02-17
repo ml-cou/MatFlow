59a7abafa5e2cfe9618ea11794f09d3735c6e477
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-02-17 11:14:58 +0600

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import time
 
 import streamlit as st
 
-st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
+st.set_page_config(layout='wide',page_title='MatFlow', initial_sidebar_state='collapsed')
 
 from subpage.navbar import navbar,vspace
 import streamlit.components.v1 as components
