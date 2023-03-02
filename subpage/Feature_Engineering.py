c3d466c7eb2f820e7228f3a5b56bf5b56d16c3e1
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-03-03 04:03:28 +0600

    Update Feature_Engineering.py

--- subpage/Feature_Engineering.py
+++ subpage/Feature_Engineering.py
@@ -2,10 +2,14 @@ import streamlit as st
 import time
 import pandas as pd
 from modules import utils
+import numpy as np
 from modules.feature import encoding, imputation, scaling, creation, dropping, change_dtype
 from modules.feature import change_fieldname
 from .navbar import vspace
+
 def ds_feature_engineering(ds):
+    if 'save_as' not in st.session_state:
+        st.session_state.save_as = True
     class funFile:
         def __init__(self, file_name, file_data):
             self.file_name = file_name
@@ -36,7 +40,7 @@ def ds_feature_engineering(ds):
         st.warning(e)
         st.stop()
 
-    menus = ["Add/Modify", "Change Dtype","Alter Field Name", "Imputation", "Encoding", "Scaling", "Drop Column","Merge Dataset"]
+    menus = ["Add/Modify", "Change Dtype","Alter Field Name", "Imputation", "Encoding", "Scaling", "Drop Column","Drop Rows","Merge Dataset"]
     tabs = [tab for tab in st.tabs(menus)]
 
     with tabs[0]:
@@ -59,32 +63,39 @@ def ds_feature_engineering(ds):
 
     with tabs[6]:
         dropping.dropping(data, data_opt)
+    with tabs[7]:
+        dropping.drop_raw(data,data_opt)
     if len(st.session_state.project_files)<=1:
-        with tabs[7]:
+        with tabs[8]:
             st.header('Please add at least two datasets !')
     else:
-        with tabs[7]:
+        with tabs[8]:
             li=[]
             for i in st.session_state.project_files:
                 if i.file_name!=ds.file_name:
                     li.append(i.file_name)
             merge_name=st.selectbox('Select Dataset You Wanna Merge With',li)
-            file_name=st.text_input('New Dataset Name',autocomplete='off')
+
+            file_name = st.text_input('New Dataset Name', autocomplete='off')
+            how = st.selectbox('How', ['left', 'right', 'outer', 'inner', 'cross'], index=3)
+            left_on = st.selectbox("Select column name for left dataframe:", ds.file_data.columns)
+            right_on = st.selectbox("Select column name for right dataframe:", st.session_state.dataset.data[merge_name].columns)
             vspace(5)
-            if st.button('Merge',type='primary'):
-                if len(file_name)==0:
+            if st.button('Merge', type='primary'):
+                if len(file_name) == 0:
                     st.error('Name can\'t be empty')
                 else:
-                    tmp=pd.DataFrame(ds.file_data)
+                    tmp = pd.DataFrame(ds.file_data)
                     for i in st.session_state.project_files:
-                        if i.file_name==merge_name:
+                        if i.file_name == merge_name:
                             try:
-                                tmp.merge(i.file_data)
+                                temp2=tmp.merge(i.file_data, left_on=left_on,right_on=right_on, how=how)
                             except Exception as e:
-                                st.error(e)
-                    if validiate_data(tmp, file_name):
-                        st.session_state.project_files.append(funFile(file_name, tmp))
+                                st.warning(e)
+                    if validiate_data(temp2, file_name):
+                        st.session_state.project_files.append(funFile(file_name, temp2))
                         st._rerun()
+
 
 
 
