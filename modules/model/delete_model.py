4a8853bb95affd4c9a9513111d71d3f3fcbda3e7
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-03-25 09:41:03 +0600

    Update delete_model.py

--- modules/model/delete_model.py
+++ modules/model/delete_model.py
@@ -1,16 +1,35 @@
-import streamlit as st  
-
+import streamlit as st
+import pickle
 from modules import utils
 
 def delete_model(models):
-	model_name = st.multiselect(
-			"Select Models",
-			models.list_name(),
-			key="delete_model"
-		)
+	# change funtion name
+	if len(models.list_name())==0:
+		st.header('No Model Found')
 
-	if st.button("Delete") and model_name:
-		models.delete_model(model_name)
+	for model_name in models.list_name():
+		st.write('#')
+		st.write('#')
+		col0,col1,col2=st.columns(3)
+		with col0:
+			st.markdown(f'<h5>{model_name}</h5>',unsafe_allow_html=True)
+		with col2:
+			if st.button("🗑️ Delete",key=model_name+'del'):
+				models.delete_model(model_name)
 
-		st.success(f"{', '.join(model_name)} Deleted!")
-		utils.rerun()
+				st.success(f"{', '.join(model_name)} Deleted!")
+				utils.rerun()
+
+		with col1:
+			# st.write(models.get_model(model_name))
+			# st.write('hi')
+			model_binary = pickle.dumps(models.get_model(model_name))
+			st.download_button(
+				label="📥 Download Model",
+				data=model_binary,
+				file_name=model_name + '.pkl',
+				mime="application/octet-stream",
+				key=model_name+'download',
+			)
+		st.markdown("<hr>", unsafe_allow_html=True)
+
