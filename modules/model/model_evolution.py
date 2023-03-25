1089b7f6944b7a21cd2f420a51418bb96ec04003
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-03-25 09:44:46 +0600

    Update model_evolution.py

--- modules/model/model_evolution.py
+++ modules/model/model_evolution.py
@@ -1,34 +1,50 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 from lazypredict.Supervised import LazyRegressor, LazyClassifier
-from modules import utils
-from sklearn.model_selection import train_test_split
-from modules.classifier import decision_tree,knn,log_reg,random_forest,svm,perceptron
+from matplotlib import pyplot as plt
+from modules.utils import split_xy
+import seaborn as sns
+
 def model_evolution():
-    # split data format : st.session_state.splitted_data = {'ytest': y_test, 'ytrain': y_train, 'Xtrain': X_train, 'Xtest': X_test,'target_var':target_var}
     try:
-        train_data = st.session_state.splitted_data['X_train']
+        train_data= st.session_state.dataset.get_data(st.session_state.splitted_data['train_name'])
+        test_data=st.session_state.dataset.get_data(st.session_state.splitted_data['test_name'])
         target_var=st.session_state.splitted_data['target_var']
+        X_train,y_train=split_xy(train_data,target_var)
+        X_test,y_test=split_xy(test_data,target_var)
+        target_var_type=train_data[target_var].dtype
     except:
-        st.header("Split Dataset First")
+        st.header("Properly Split Dataset First")
         return
 
-    # Load data
-    data = train_data
     st.write("ML type: ")
     col1, col2 = st.columns([4, 4])
     options = ["Regresion", "Classification"]
 
-    if data[target_var].dtype == "float64" or data[target_var].dtype == "int64":
+    if target_var_type == "float64" or target_var_type == "int64":
         st.write(f"{target_var} is numerical")
         default_option = "Regresion"
         with col1:
             selected_option = st.radio("", options, index=options.index(default_option))
 
-
         reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
-        models, predictions = reg.fit(st.session_state.splitted_data['X_train'], st.session_state.splitted_data['X_test'], st.session_state.splitted_data['y_train'], st.session_state.splitted_data['y_test'])
-        with col1:
-            st.table(models)
+        models, predictions = reg.fit(X_train, X_test,y_train, y_test)
+        st.dataframe(models)
+
+        r2_scores = {}
+        for key, value in models.items():
+            r2_scores[key] = models.score(X_test, y_test)
+
+        plt.figure(figsize=(10, 5))
+        plt.xticks(rotation=90)
+        sns.barplot(x=list(r2_scores.values()), y=list(r2_scores.keys()))
+        plt.xlabel('R-squared score')
+        plt.title('LazyRegressor Model Comparison')
+        plt.tight_layout()
+        plt.show()
+
+        # st.write(predictions)
 
     else:
         default_option = "Classification"
@@ -37,35 +53,12 @@ def model_evolution():
             st.write(f"{target_var} is not numerical")
 
         clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
-        models, predictions = clf.fit(st.session_state.splitted_data['X_train'], st.session_state.splitted_data['X_test'], st.session_state.splitted_data['y_train'], st.session_state.splitted_data['y_test'])
-        results_df = models
+        models, predictions = clf.fit(X_train, X_test,y_train, y_test)
+        st.dataframe(models)
+        fig, ax = plt.subplots()
+        ax.barh(models.index, models['Accuracy'])
+        ax.set_xlabel('Accuracy')
+        ax.set_title('Model Performance')
+        st.pyplot(fig)
 
-        # Create a selectbox to choose a column from the dataframe
-        # selected_column = st.selectbox('Select a column', options=results_df.columns)
-
-        # Get the values of the selected column
-        # selected_column_values = results_df[selected_column].tolist()
-
-        # Display the values of the selected column
-        # st.write(selected_column_values)
-        idx = models.head()
-        # st.write(idx.index.values)
-        listCls = []
-        # with col1:
-        st.write(models.index)
-        for i in models.index:
-            if (i == "decision_tree"):
-                listCls.insert(i, decision_tree())
-            elif (i == "knn"):
-                listCls.insert(i, knn())
-            elif (i == "log_reg"):
-                listCls.insert(i, log_reg())
-            elif (i == "perceptron"):
-                listCls.insert(i, perceptron())
-            elif (i == "random_forest"):
-                listCls.insert(i, random_forest())
-            elif (i == "svm"):
-                listCls.insert(i, svm())
-
-        models.insert(loc=5, column="Model name", value=[i % 7 + 1 for i in range(len(models.index))])
-        st.write(models)
+        # st.write(predictions)
