b26db893c4794e54e4655e1c4ea820dc3f6c9eb3
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-02 13:17:46 +0600

    Create lasso_regression.py

---
+++ modules/regressor/lasso_regression.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from sklearn.linear_model import Lasso
+
+def lasso_regression():
+    alpha = st.number_input("Alpha", 0.0, 100.0, 1.0, key="ls_alpha")
+    fit_intercept = st.checkbox("Fit Intercept", True, key="ls_fit_intercept")
+    normalize = st.checkbox("Normalize", False, key="ls_normalize")
+    max_iter = st.number_input("Max Iterations", 1, 100000, 1000, key="ls_max_iter")
+    selection = st.selectbox("Selection", ["cyclic", "random"], index=0, key="ls_selection")
+
+    model = Lasso(alpha=alpha, fit_intercept=fit_intercept, normalize=normalize, max_iter=max_iter, selection=selection)
+
+    return model
