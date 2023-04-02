be03464ee9e7b8b8f40d59022abcf9dbdce3934d
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-02 13:25:01 +0600

    Update model_builder.py

--- subpage/model_builder.py
+++ subpage/model_builder.py
@@ -3,13 +3,8 @@ import pandas as pd
 
 from modules import utils
 from modules.classes import model
-from modules.model import build_model, model_report, prediction, delete_model,split_dataset,model_evolution,feature_selection,classification
+from modules.model import build_model, model_report, prediction, delete_model,split_dataset,model_evaluation,feature_selection,classification,regression
 def model_builder(dataset,table_name):
-	try:
-		models = st.session_state["models"]
-	except:
-		st.session_state["models"] = model.Classification()
-		models = st.session_state["models"]
 
 	try:
 		dataset = st.session_state["dataset"]
@@ -22,26 +17,45 @@ def model_builder(dataset,table_name):
 		st.warning(e)
 		return
 
-	menus = ["Split Dataset","Model Evolution","Feature Selection","Build Model", "Model Report", "Model Prediction", "Models"]
+	menus = ["Split Dataset","Build Model", "Model Report", "Model Prediction","Model Evaluation", "Models"]
 	tabs = st.tabs(menus)
-	print(models)
 	#
+
 	with tabs[0]:
-		split_dataset.split_dataset(dataset,table_name, models)
+		split_dataset.split_dataset(dataset,table_name)
 	#
+	with tabs[4]:
+		model_evaluation.model_evaluation()
 	with tabs[1]:
-		model_evolution.model_evolution()
-	#
-	# with tabs[2]:
-	# 	prediction.prediction(dataset, models)
-	with tabs[3]:
 		if 'splitted_data' in st.session_state:
-			classification.classification(dataset,models)
+			if st.session_state.splitted_data['type']=='Classifier':
+				try:
+					models = st.session_state["models"]
+				except:
+					st.session_state["models"] = model.Classification()
+					models = st.session_state["models"]
+				classification.classification(dataset,models)
+			else:
+				try:
+					models = st.session_state["models"]
+				except:
+					st.session_state["models"] = model.Regressor()
+					models = st.session_state["models"]
+				regression.regression(dataset,models)
 		else:
 			st.header('Split Dataset First')
-	with tabs[4]:
-		model_report.model_report(models)
+	with tabs[2]:
+		if "models" in st.session_state:
+			model_report.model_report(models)
+		else:
+			st.header('No Model Found')
+	with tabs[3]:
+		if "models" in st.session_state:
+			prediction.prediction(dataset, models)
+		else:
+			st.header('No Model Found')
 	with tabs[5]:
-		prediction.prediction(dataset, models)
-	with tabs[6]:
-		delete_model.delete_model(models)
+		if "models" in st.session_state:
+			delete_model.delete_model(models)
+		else:
+			st.header('No Model Found')
