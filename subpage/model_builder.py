3d91a58452bc19ec38cf439338448ffa0b460974
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-08 00:14:24 +0600

    Update model_builder.py

--- subpage/model_builder.py
+++ subpage/model_builder.py
@@ -3,74 +3,79 @@ import pandas as pd
 
 from modules import utils
 from modules.classes import model
-from modules.model import  model_report, prediction_classification,prediction_regression, delete_model,split_dataset,model_evaluation,feature_selection,classification,regression
-def model_builder(dataset,table_name):
+from modules.model import model_report, prediction_classification, prediction_regression, delete_model, split_dataset, \
+    model_evaluation, feature_selection, classification, regression
 
-	try:
-		dataset = st.session_state["dataset"]
 
-	except KeyError:
-		st.header("No Dataset Found")
-		return
+def model_builder(dataset, table_name):
+    try:
+        dataset = st.session_state["dataset"]
 
-	except Exception as e:
-		st.warning(e)
-		return
+    except KeyError:
+        st.header("No Dataset Found")
+        return
 
-	menus = ["Split Dataset","Build Model", "Model Report", "Model Prediction","Model Evaluation", "Models"]
-	tabs = st.tabs(menus)
-	#
+    except Exception as e:
+        st.warning(e)
+        return
 
-	with tabs[0]:
-		split_dataset.split_dataset(dataset,table_name)
-	with tabs[1]:
-		if 'splitted_data' in st.session_state:
-			split_name=st.selectbox('Select Train Test Dataset',st.session_state.splitted_data.keys())
-			if st.session_state.splitted_data[split_name]['type']=='Classification':
-				try:
-					models = st.session_state["models"]
-				except:
-					st.session_state["models"] = model.Classification()
-					models = st.session_state["models"]
-				classification.classification(st.session_state.splitted_data[split_name],models)
-			else:
-				try:
-					models = st.session_state["models"]
-				except:
-					st.session_state["models"] = model.Regressor()
-					models = st.session_state["models"]
-				regression.regression(st.session_state.splitted_data[split_name],models)
-		else:
-			st.header('Split Dataset First')
-	with tabs[2]:
-		if "models" in st.session_state:
+    menus = ["Split Dataset", "Build Model", "Model Report", "Model Prediction", "Model Evaluation", "Models"]
+    tabs = st.tabs(menus)
+    #
 
-			model_report.model_report(models)
-		else:
-			st.header('No Model To Report')
-	with tabs[3]:
-		## try catch needed here
-		if "all_models" in st.session_state:
-			all_models=st.session_state.all_models
+    with tabs[0]:
+        split_dataset.split_dataset(dataset, table_name)
+    with tabs[1]:
+        if 'splitted_data' in st.session_state:
+            split_name = st.selectbox('Select Train Test Dataset', st.session_state.splitted_data.keys())
+            if st.session_state.splitted_data[split_name]['type'] == 'Classification':
+                try:
+                    models = st.session_state["models"]
+                except:
+                    st.session_state["models"] = model.Classification()
+                    models = st.session_state["models"]
+                classification.classification(st.session_state.splitted_data[split_name], models)
+            else:
+                try:
+                    models = st.session_state["models"]
+                except:
+                    st.session_state["models"] = model.Regressor()
+                    models = st.session_state["models"]
+                regression.regression(st.session_state.splitted_data[split_name], models)
+        else:
+            st.header('Split Dataset First')
+    with tabs[2]:
+        if "models" in st.session_state:
 
-			model_name=st.selectbox(
-				"Select Model",
-				all_models.keys()
-			)
+            model_report.model_report(models)
+        else:
+            st.header('No Model To Report')
+    with tabs[3]:
+        ## try catch needed here
+        if "all_models" in st.session_state:
+            all_models = st.session_state.all_models
 
-			if all_models[model_name][0] == 'Classification':
-				prediction_classification.prediction(dataset, models,model_name)
-			else:
-				prediction_regression.prediction(dataset,models,model_name)
-		else:
-			st.header('No Model Found')
-	with tabs[4]:
-		if 'all_models' not in st.session_state:
-			st.header('Create a Model First')
-		else:
-			model_evaluation.model_evaluation()
-	with tabs[5]:
-		if "models" in st.session_state:
-			delete_model.delete_model(models)
-		else:
-			st.header('No Model Found')
+            model_name = st.selectbox(
+                "Select Model",
+                all_models.keys()
+            )
+
+            if all_models[model_name][0] == 'Classification':
+                prediction_classification.prediction(dataset, models, model_name)
+            else:
+                prediction_regression.prediction(dataset, models, model_name)
+        else:
+            st.header('No Model Found')
+    with tabs[4]:
+        if 'splitted_data' in st.session_state:
+            split_name = st.selectbox('Select Train Test Dataset', st.session_state.splitted_data.keys(),
+                                      key='evaluation')
+            if split_name:
+                model_evaluation.model_evaluation(st.session_state.splitted_data[split_name])
+        else:
+            st.header('Split Dataset First')
+    with tabs[5]:
+        if "models" in st.session_state:
+            delete_model.delete_model(models)
+        else:
+            st.header('No Model Found')
