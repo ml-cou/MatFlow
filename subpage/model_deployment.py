61f19c7aa7e098061b6c6adbb3bdd4e948900d1f
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-08 01:34:17 +0600

    Update model_deployment.py

--- subpage/model_deployment.py
+++ subpage/model_deployment.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import pandas as pd
+
+
 def model_deployment():
     if 'all_models' not in st.session_state:
         st.header('Create a Model First')
@@ -7,7 +8,7 @@ def model_deployment():
 
     all_models = st.session_state.all_models
 
-    col1,col2,col3,col4=st.columns([4,0.5,2,0.5])
+    col1, col2, col3, col4 = st.columns([4, 0.5, 2, 0.5])
     with col1:
         model_name = st.selectbox('Select Model', all_models, key='model_evaluation')
 
@@ -16,55 +17,45 @@ def model_deployment():
     except:
         st.header("Properly Split Dataset First")
         return
-    
+
     try:
-        model=st.session_state["models"]
-        train_data_name=dataset['train_name']
-        train_data=st.session_state.dataset.get_data(train_data_name)
-        target_var=dataset['target_var']
+        model = st.session_state["models"]
+        train_data_name = dataset['train_name']
+        train_data = st.session_state.dataset.get_data(train_data_name)
+        target_var = dataset['target_var']
     except:
         st.header('Create a Model First')
         return
 
-    col_names_all=[]
+    col_names_all = []
     for i in train_data.columns:
-        if i==target_var:
+        if i == target_var:
             continue
         col_names_all.append(i)
 
-    rad=col3.radio('Select Columns',['All Columns', 'Custom Columns'])
-    if rad=='All Columns':
-        col_names=col_names_all
+    rad = col3.radio('Select Columns', ['All Columns', 'Custom Columns'])
+    if rad == 'All Columns':
+        col_names = col_names_all
     else:
-        col_names=st.multiselect('Custom Columns',col_names_all,help='Other values will be 0 as default value')
+        col_names = st.multiselect('Custom Columns', col_names_all, help='Other values will be 0 as default value')
 
-
-    col1,col2,col3,col4=st.columns([4,0.5,2,0.5])
-    prediction=['']
+    col1, col2, col3, col4 = st.columns([4, 0.5, 2, 0.5])
+    prediction = ['']
 
     with col1:
         st.header('INPUT')
         for i in col_names:
-            st.number_input(i,key=i)
+            st.number_input(i, key=i)
         st.write('#')
-        if st.button('Submit',type="primary"):
-            X=[]
-            for i in col_names:
-                X.append(st.session_state[i])
-
-            prediction=model.get_prediction(model_name,[X])
+        if st.button('Submit', type="primary"):
+            X = [st.session_state[i] if i in col_names else 0 for i in col_names_all]
+            prediction = model.get_prediction(model_name, [X])
     with col3:
         st.header('PREDICTION')
         st.header('#')
         st.write('#')
         st.write('#')
         st.write('#')
-        st.markdown(f'<h4> { target_var.upper() } : { prediction[0] }</h4>',
+        st.markdown(f'<h4> {target_var.upper()} : {prediction[0]}</h4>',
                     unsafe_allow_html=True
                     )
-
-
-
-
-
-
