0b1f6e75182b7255f25d313c1585e1d37212e66f
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-14 12:15:30 +0600

    Update model.py

--- modules/classes/model.py
+++ modules/classes/model.py
@@ -1,65 +1,50 @@
 import pandas as pd
-import streamlit
 
 
-class Classification:
-	def __init__(self):
-		self.model = {}
-		self.result = pd.DataFrame(columns=[
-				"Model Name", "Train Data", "Test Data", 
-				"Train Accuracy", "Train Precision", "Train Recall", "Train F1-Score",
-				"Test Accuracy", "Test Precision", "Test Recall", "Test F1-Score"
-			])
-		self.target_var = []
-	def get_model(self,model_name):
-		return self.model[model_name]
-
-	def add_model(self, model_name, model, train_name, test_name, target_var, result):
-		self.model[model_name] = model
-		if target_var not in self.target_var:
-			self.target_var.append(target_var)
-
-		model_info = [model_name, train_name, test_name]
-		new_idx = len(self.result)
-		self.result.loc[new_idx] = model_info + result
-
-	def get_prediction(self, model_name, X):
-		return self.model[model_name].predict(X)
-
-
-	def list_name(self):
-		if bool(self.model):
-			return list(self.model.keys())
-		return []
-
-	def delete_model(self, model_name):
-		del self.model[model_name]
-		drop_idx = self.result.index[self.result["Model Name"] == model_name][0]
-		self.result = self.result.drop(drop_idx).reset_index(drop=True)
-
-class Regressor:
+class Models:
     def __init__(self):
         self.model = {}
-        self.result = pd.DataFrame(columns=[
-                "Model Name", "Train Data", "Test Data",
-                "Train R-Squared", "Train Mean Absolute Error", "Train Mean Squared Error", "Train Root Mean Squared Error",
-                "Test R-Squared", "Test Mean Absolute Error", "Test Mean Squared Error", "Test Root Mean Squared Error"
-            ])
+        self.result={}
         self.target_var = []
 
     def get_model(self, model_name):
         return self.model[model_name]
 
-    def add_model(self, model_name, model, train_name, test_name, target_var, result):
+    def get_result(self,model_name):
+        return self.result[model_name]
 
+
+
+    def add_model(self, model_name, model, train_name, test_name, target_var, result, model_type):
         self.model[model_name] = model
-        if target_var not in self.target_var:
-            self.target_var.append(target_var)
+
+        self.target_var.append(target_var)
+
+        classification_result = pd.DataFrame(columns=[
+            "Model Name", "Train Data", "Test Data",
+            "Train Accuracy", "Train Precision", "Train Recall", "Train F1-Score",
+            "Test Accuracy", "Test Precision", "Test Recall", "Test F1-Score"
+        ])
+
+        regression_result = pd.DataFrame(columns=[
+            "Model Name", "Train Data", "Test Data",
+            "Train R-Squared", "Train Mean Absolute Error", "Train Mean Squared Error", "Train Root Mean Squared Error",
+            "Test R-Squared", "Test Mean Absolute Error", "Test Mean Squared Error", "Test Root Mean Squared Error"
+        ])
+
+        if model_type == "classification":
+            temp_result = classification_result
+        elif model_type == "regression":
+            temp_result = regression_result
+        else:
+            raise ValueError("Invalid model type. Must be either 'classification' or 'regression'.")
 
         model_info = [model_name, train_name, test_name]
-        new_idx = len(self.result)
-        print('hrere',result)
-        self.result.loc[new_idx] = model_info + result
+        new_idx = len(temp_result)
+        temp_result.loc[new_idx] = model_info + result
+
+        self.result[model_name]=temp_result
+
 
     def get_prediction(self, model_name, X):
         return self.model[model_name].predict(X)
@@ -71,5 +56,9 @@ class Regressor:
 
     def delete_model(self, model_name):
         del self.model[model_name]
-        drop_idx = self.result.index[self.result["Model Name"] == model_name][0]
-        self.result = self.result.drop(drop_idx).reset_index(drop=True)
+        drop_idx = self.classification_result.index[self.classification_result["Model Name"] == model_name]
+        if len(drop_idx) > 0:
+            self.classification_result = self.classification_result.drop(drop_idx[0]).reset_index(drop=True)
+        else:
+            drop_idx = self.regression_result.index[self.regression_result["Model Name"] == model_name]
+            self.regression_result = self.regression_result.drop(drop_idx[0]).reset_index(drop=True)
