3e292313eb398f47de05e58008401cbd94e71eee
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-14 12:16:20 +0600

    Update decision_tree.py

--- modules/classifier/decision_tree.py
+++ modules/classifier/decision_tree.py
@@ -1,62 +1,131 @@
-import streamlit as st  
+import time
 
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
 
-def decision_tree():
-	max_depth = None
-	col1, col2, col3 = st.columns(3)
-	criterion = col1.selectbox(
-			"Criterion",
-			["gini", "entropy", "log_loss"],
-			0, 
-			key="dt_criterion"
-		)
+def hyperparameter_optimization(X_train, y_train):
+    st.subheader("Hyperparameter Optimization Settings")
+    n_iter = st.number_input("Number of iterations for hyperparameter search", min_value=1, value=10, step=1)
+    cv = st.number_input("Number of cross-validation folds", min_value=2, value=5, step=1)
+    random_state = st.number_input("Random state for hyperparameter search", min_value=0, value=0, step=1)
 
-	min_samples_split = col2.number_input(
-			"Min. Samples Split",
-			2, 20, 2,
-			key="dt_min_samples_split"
-		)
+    st.write('#')
 
-	min_samples_leaf = col3.number_input(
-			"Min. Samples Leaf",
-			1, 20, 1,
-			key="dt_min_samples_leaf"
-		)
+    if st.button('Run Optimization'):
 
-	col1, col2, col3, _ = st.columns([2,1.33,3.33,3.33])
-	col2.markdown("#")
-	auto_max_depth = col2.checkbox("None", True, key="dt_auto_max_depth")
-	if auto_max_depth:
-		max_depth = col1.text_input(
-			"Max Depth",
-			None,
-			key="dt_max_depth_none",
-			disabled=True
-		)
-	else:
-		max_depth = col1.number_input(
-			"Max Depth",
-			1, 20, 7,
-			key="dt_max_depth"
-		)
+        st.write('#')
+        param_dist = {
+            "criterion": ["gini", "entropy"],
+            "splitter": ["best", "random"],
+            "max_depth": [3, 5, 10, 15, 20, 25, None],
+            "min_samples_split": [2, 5, 10],
+            "min_samples_leaf": [1, 2, 4],
+            "random_state": [random_state]
+        }
+        model = DecisionTreeClassifier()
 
-	random_state = col3.number_input(
-			"Random State",
-			0, 1000000, 0,
-			key="lr_random_state"
-		)
+        with st.spinner('Doing hyperparameter optimization...'):
+            for i in range(100):
+                time.sleep(0.1)
+                st.spinner(f"Running iteration {i + 1} of {n_iter}...")
+            clf = RandomizedSearchCV(model, param_distributions=param_dist, n_iter=n_iter, cv=cv,
+                                     random_state=random_state)
+            st.spinner("Fitting the model...")
+            clf.fit(X_train, y_train)
 
-	max_depth = None if auto_max_depth else max_depth
+        st.success("Hyperparameter optimization completed!")
 
-	try:
-		model = DecisionTreeClassifier(criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split,
-									   min_samples_leaf=min_samples_leaf, random_state=random_state)
-	except ValueError as e:
-		print("ValueError occurred while initializing the model:", e)
-	except TypeError as e:
-		print("TypeError occurred while initializing the model:", e)
-	except:
-		print("An error occurred while initializing the model.")
+        # st.write(f"Best estimator:")
+        best_params = clf.best_params_
+        # st.write(pd.DataFrame.from_dict(best_params, orient='index', columns=['Value']))
+        st.write('Best estimator:')
 
-	return model
+        rows = []
+        for param in best_params:
+            rows.append([param, best_params[param]])
+        st.table(pd.DataFrame(rows, columns=['Parameter', 'Value']))
+
+        # st.write(clf.best_estimator_)
+        return clf.best_estimator_
+
+
+
+def decision_tree(X_train, y_train):
+    do_hyperparameter_optimization = st.checkbox("Do Hyperparameter Optimization?")
+
+    if do_hyperparameter_optimization:
+        hyperparameter_optimization(X_train, y_train)
+
+    st.subheader("Model Settings")
+
+    max_depth = None
+    col1, col2, col3 = st.columns(3)
+    criterion = col1.selectbox(
+        "Criterion",
+        ["gini", "entropy", "log_loss"],
+        0,
+        key="dt_criterion"
+    )
+
+    min_samples_split = col2.number_input(
+        "Min. Samples Split",
+        2, 20, 2,
+        key="dt_min_samples_split"
+    )
+
+    min_samples_leaf = col3.number_input(
+        "Min. Samples Leaf",
+        1, 20, 1,
+        key="dt_min_samples_leaf"
+    )
+
+    col1, col2, col3, _ = st.columns([2, 1.33, 3.33, 3.33])
+    col2.markdown("#")
+    auto_max_depth = col2.checkbox("None", True, key="dt_auto_max_depth")
+    if auto_max_depth:
+        max_depth = col1.text_input(
+            "Max Depth",
+            None,
+            key="dt_max_depth_none",
+            disabled=True
+        )
+    else:
+        max_depth = col1.number_input(
+            "Max Depth",
+            1, 20, 7,
+            key="dt_max_depth"
+        )
+
+    random_state = col3.number_input(
+        "Random State",
+        0, 100, 0,
+        key="lr_random_state"
+    )
+
+    max_depth = None if auto_max_depth else max_depth
+
+    try:
+        model = DecisionTreeClassifier(criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split,
+                                       min_samples_leaf=min_samples_leaf, random_state=random_state)
+
+        # Hyperparameter optimization
+        n_iter = 50
+        cv = 5
+
+
+        best_model = hyperparameter_optimization(X_train, y_train, n_iter, cv, random_state)
+
+        st.write(best_model)
+        # Use the best model obtained from hyperparameter optimization
+        # model.set_params(**best_model.get_params())
+
+    except ValueError as e:
+        print("ValueError occurred while initializing the model:", e)
+    except TypeError as e:
+        print("TypeError occurred while initializing the model:", e)
+    except:
+        print("An error occurred while initializing the model.")
+
+    return model
