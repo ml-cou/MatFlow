d85a3fefbde83a64c2b52dc058a709aa668e1ca9
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-04-14 12:17:09 +0600

    Update decision_tree_regression.py

--- modules/regressor/decision_tree_regression.py
+++ modules/regressor/decision_tree_regression.py
@@ -1,7 +1,59 @@
-import streamlit as st
-from sklearn.tree import DecisionTreeRegressor
+import time
 
-def decision_tree_regressor():
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.tree import DecisionTreeRegressor
+def hyperparameter_optimization(X_train, y_train):
+    st.subheader("Hyperparameter Optimization Settings")
+    n_iter = st.number_input("Number of iterations for hyperparameter search", min_value=1, value=10, step=1)
+    cv = st.number_input("Number of cross-validation folds", min_value=2, value=5, step=1)
+    random_state = st.number_input("Random state for hyperparameter search", min_value=0, value=0, step=1)
+
+    st.write('#')
+
+    if st.button('Run Optimization'):
+
+        st.write('#')
+        param_dist = {
+            "max_depth": [3, 5, 10, 15, 20, None],
+            "min_samples_split": [2, 5, 10],
+            "min_samples_leaf": [1, 2, 4],
+            "max_features": ["sqrt", "log2", None],
+            "criterion": ["mse", "friedman_mse", "mae"],
+            "random_state": [random_state],
+        }
+        model = DecisionTreeRegressor()
+
+        with st.spinner('Doing hyperparameter optimization...'):
+            for i in range(100):
+                time.sleep(0.1)
+                st.spinner(f"Running iteration {i + 1} of {n_iter}...")
+            clf = RandomizedSearchCV(model, param_distributions=param_dist, n_iter=n_iter, cv=cv,
+                                     random_state=random_state)
+            st.spinner("Fitting the model...")
+            clf.fit(X_train, y_train)
+
+        st.success("Hyperparameter optimization completed!")
+        best_params = clf.best_params_
+
+        st.write('Best estimator:')
+
+        rows = []
+        for param in best_params:
+            rows.append([param, best_params[param]])
+        st.table(pd.DataFrame(rows, columns=['Parameter', 'Value']))
+
+        return clf.best_estimator_
+
+def decision_tree_regressor(X_train, y_train):
+    do_hyperparameter_optimization = st.checkbox("Do Hyperparameter Optimization?")
+
+    if do_hyperparameter_optimization:
+        hyperparameter_optimization(X_train, y_train)
+
+    st.subheader("Model Settings")
+
     max_depth = None
     col1, col2, col3 = st.columns(3)
     criterion = col1.selectbox(
