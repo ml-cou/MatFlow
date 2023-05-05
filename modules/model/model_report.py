f23cdec7414832c9819b69bdca73180097a42d50
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-05-05 09:17:30 +0600

    Update model_report.py

--- modules/model/model_report.py
+++ modules/model/model_report.py
@@ -17,9 +17,7 @@ def model_report(split_dataset_name, models):
     if display_type == "Table":
         col2.markdown("#")
         include_data = col2.checkbox("Include Data")
-
         report_table(result_df, include_data)
-
     else:
         report_graph(result_df, col2)
 
@@ -58,21 +56,51 @@ def report_table(result_df, include_data):
         )
     st.dataframe(result_df[cols])
 
-
 def report_graph(data, col):
     model_data = data.drop(columns=['Train Data', 'Test Data', 'Model Name'])
-
     cmap = plt.cm.get_cmap('Set3', len(model_data))
-
+    result_df = model_data
+    column=pd.DataFrame()
     orientation = st.selectbox('Select Orientation', ['Vertical', 'Horizontal'])
-    for i in range(4):
+    for i in range(2):
         st.write('#')
+    display_result = st.radio(
+        "Display Result",
+        ["All", "Train", "Test", "Custom"],
+        index=2,
+        horizontal=True
+    )
+    if display_result == "All":
+        column = model_data
+
+    elif display_result == "Train":
+        colms = result_df.columns[result_df.columns.str.contains("Train")].to_list()
+        column = model_data[colms]
+
+    elif display_result == "Test":
+        colms = result_df.columns[result_df.columns.str.contains("Test")].to_list()
+        column = model_data[colms]
+
+    elif display_result == "Custom":
+        try:
+            selected_columns = st.multiselect(
+                "Columns",
+                model_data.columns,
+                []
+            )
+            if len(selected_columns) > 0:
+                column = model_data[selected_columns]
+            else :
+                st.warning("Please select at least one column")
+        except ValueError:
+            st.warning("Please select at least one column")
+
     if orientation == 'Vertical':
-        fig, ax = plt.subplots(nrows=1, ncols=len(model_data.columns), figsize=(16,8))
+        fig, ax = plt.subplots(nrows=1, ncols=len(column.columns), figsize=(16,8))
         fig.subplots_adjust(hspace=0.5, wspace=0.5)
 
-        for i, col in enumerate(model_data.columns):
-            for j, row in enumerate(model_data.iterrows()):
+        for i, col in enumerate(column.columns):
+            for j, row in enumerate(column.iterrows()):
                 # Create the bar plot for the current column
                 ax[i].bar(j, row[1][i], color=cmap(j), label=list(data['Model Name'].values)[j])
                 ax[i].set_xticklabels([])
@@ -82,7 +110,7 @@ def report_graph(data, col):
         fig.subplots_adjust(top=0.85 + 0.05 * len(data))
 
     elif orientation == 'Horizontal':
-        fig, ax = plt.subplots(nrows=len(model_data.columns), ncols=1, figsize=(7,16+len(model_data.columns)))
+        fig, ax = plt.subplots(nrows=len(model_data.columns), ncols=1, figsize=(10,8+len(model_data.columns)))
         fig.subplots_adjust(hspace=0.5, wspace=0.5)
         for i, col in enumerate(model_data.columns):
             for j, row in enumerate(model_data.iterrows()):
